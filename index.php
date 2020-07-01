4e2aa92ae670ada83f90591914f0fcbb58919e38
bricelagroy <brice.lagroy@gmail.com>
2020-07-01 10:11:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ $myText = "coucou";
 ?>
 <html>
 <head>
-<!-- add js tag here -->
+  <!-- add js tag here -->
   <script>
     const addItem = () => { 
       let transactionItems = document.getElementById('transaction_items');
       const clone = transactionItems.children[0].cloneNode(true);
       transactionItems.appendChild(clone);
       for(let i = 0; i < transactionItems.childElementCount ; i++) {
-        transactionItems.children[i].getElementsByClassName('item_value').setAttribute("name", "item_value_" + i);
+        transactionItems.children[i].getElementsByClassName('item_value')[0].setAttribute("name", "item_value_" + i);
       }
     };
   </script>
@@ -18,7 +18,10 @@ $myText = "coucou";
 <body>
   <div>
   </div>
-<form id="transaction_form" action="/redirection.html">
+<form id="transaction_form" action="/connfirmation.html">
+  <div id="action_tracking">
+    <button>Useless button (for action tracking)</button>
+  </div>
   <div id="transaction_items">
     <div> 
       <label>Item Price:</label><br>
@@ -26,7 +29,7 @@ $myText = "coucou";
     </div>
   </div>
   <button type="button" onclick="addItem();">Add item</button> 
-  <input type="submit" value="Submit">
+  <input type="submit" value="Buy">
 </form>
 </body>
 </html>
