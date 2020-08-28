f2c6dd5290d950ea64e14c2ffd38fa79cd4301dd
bricelagroy <brice@abtasty.com>
2020-08-28 11:36:17 +0200

    add geoloc info

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
 $myText = "Buy";
+
+
+function getVisIpAddr() {
+
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    }
+    else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
+
+// Store the IP address
+$vis_ip = getVisIPAddr();
+
+// Use JSON encoded string and converts
+// it into a PHP variable
+$ipdat = @json_decode(file_get_contents(
+    "http://www.geoplugin.net/json.gp?ip=" . $vis_ip));
+
+$geolocInfo =  'Country Name: ' . $ipdat->geoplugin_countryName . "\n";
+$geolocInfo .= 'City Name: ' . $ipdat->geoplugin_city . "\n";
+$geolocInfo .= 'Continent Name: ' . $ipdat->geoplugin_continentName . "\n";
+$geolocInfo .= 'Latitude: ' . $ipdat->geoplugin_latitude . "\n";
+$geolocInfo .= 'Longitude: ' . $ipdat->geoplugin_longitude . "\n";
+$geolocInfo .= 'Currency Symbol: ' . $ipdat->geoplugin_currencySymbol . "\n";
+$geolocInfo .= 'Currency Code: ' . $ipdat->geoplugin_currencyCode . "\n";
+$geolocInfo .= 'Timezone: ' . $ipdat->geoplugin_timezone;
+
 ?>
 <html>
 <head>
@@ -29,5 +61,11 @@ $myText = "Buy";
   <button type="button" onclick="addItem();">Add item</button> 
   <input type="submit" value="<?php echo $myText; ?>">
 </form>
+<p>
+    Geoloc  infos:
+</p>
+<p>
+<?php echo $geolocInfo ?>
+</p>
 </body>
 </html>
