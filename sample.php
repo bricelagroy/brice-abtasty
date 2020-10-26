a719498d0c73dbfb7cb8a715bdd0c7145e84fd5f
bricelagroy <brice@abtasty.com>
2020-10-26 18:26:57 +0100

    add header

--- sample.php
+++ sample.php
@@ -1,5 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, HEAD, OPTIONS, POST,  PUT");
 header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Headers: Authorization");
+
 die(file_get_contents('sample.json'));
