042c3aeadc1e1de153d5227c05bf7493041473cd
bricelagroy <brice@abtasty.com>
2020-10-26 18:16:35 +0100

    test another origin

--- sample.php
+++ sample.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: https://app2.abtasty.com');
+header('Access-Control-Allow-Origin: https://api-reporting.abtasty.com');
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header('Content-Type: application/json; charset=utf-8');
 die(file_get_contents('sample.json'));
