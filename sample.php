561e22a6ebd6c48e81c1a13db237bc3eb8f01436
bricelagroy <brice@abtasty.com>
2020-10-26 18:14:19 +0100

    CORS add GET method

--- sample.php
+++ sample.php
@@ -1,4 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: https://app2.abtasty.com');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 header('Content-Type: application/json; charset=utf-8');
 die(file_get_contents('sample.json'));
