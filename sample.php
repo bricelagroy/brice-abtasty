0e22c9fa4a31948ae1dc1cda3f0845f08261d0ca
bricelagroy <brice@abtasty.com>
2020-10-26 18:13:31 +0100

    CORS

--- sample.php
+++ sample.php
@@ -1,4 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://app2.abtasty.com');
 header('Content-Type: application/json; charset=utf-8');
 die(file_get_contents('sample.json'));
